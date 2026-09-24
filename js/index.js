23676da7d1a54da8cacb192b73b00430dd034513
Ahmed Rashad <ahmedrashad384@gmail.com>
2026-09-25 01:17:17 +0300

    finalize first phase from the js logic

--- js/index.js
+++ js/index.js
@@ -186,13 +186,20 @@ var notes = document.getElementById("noteInput");
 var rowData = document.getElementById("addButnRowData");
 var addingTotalContact = document.getElementById("paragraphCount");
 var addTotalContactInHeader = document.getElementById("total-contacts");
+var imagePathOrIntials = document.getElementById("intialLetters ");
+var saveBtn = document.getElementById("saveContact");
+var updateBtn = document.getElementById("saveUpdate");
 
-var contactList = [];
-var totalNumberOfContacts = 0;
+var contactList = JSON.parse(localStorage.getItem("Contacts")) || [];
+displayContactList();
+
+//
 
 function addNewContact() {
   var newContact = {
-    image: contactImage.value,
+    // image: contactImage.files[0]
+    //   ? `imgs/${contactImage.files[0].name}`
+    //   : `<span class="first-letter">A<span class="second-letter">R</span></span>`,
     name: fullName.value,
     number: phoneNumber.value,
     email: emailAddress.value,
@@ -203,7 +210,9 @@ function addNewContact() {
   };
 
   contactList.push(newContact);
+  localStorage.setItem("Contacts", JSON.stringify(contactList));
   displayContactList();
+
   clearAllInputs();
   console.log(contactList);
 
@@ -232,7 +241,7 @@ function clearAllInputs() {
 
 function displayContactList() {
   var box = "";
-  totalNumberOfContacts++;
+  // totalNumberOfContacts++;
 
   for (var i = 0; i < contactList.length; i++) {
     box += `<div class="col-12 col-md-6">
@@ -240,8 +249,9 @@ function displayContactList() {
                           <div class="contact-card my-3 shadow-sm rounded-5 overflow-hidden">
                             <div class="contact-info p-3 bg-white pb-2">
                               <div class="contact-heading d-flex gap-3">
-                                <div class="intial-letters font-18 fw-semibold name-gradient p-3 rounded-3 text-white mb-2">
-                                  <span class="first-letter">A<span class="second-letter">R</span></span>
+                                <div class="intial-letters font-18 fw-semibold name-gradient p-3 rounded-3 text-white mb-2" id="intialLetters">
+                                  <!-- <img src="${contactList[i].image}" alt=""  class="z-3 w-100 h-100"/> -->
+                                 <span class="first-letter">A<span class="second-letter">R</span></span>   
                                 </div>
                                 <div class="name-number">
                                   <h5 class="name text-capitalize m-0 fs-6 fw-semibold mb-2" id="contactName">${contactList[i].name}</h5>
@@ -278,8 +288,9 @@ function displayContactList() {
                                 <button class="add-emergency rounded-2" id="addEmergency">
                                   <span class="d-block font-14 p-2 rounded-2 red-icon"><i class="fa-solid fa-heart-pulse"></i></span>
                                 </button>
-                                <button class="edit text-muted p-2" id="editContact"><i class="fa-solid fa-pen"></i></button>
-                                <button class="delete text-muted p-2" id="deleteContact"><i class="fa-solid fa-trash"></i></button>
+                                   <!-- delete and update buttons -->
+                                <button onclick="editContact (${i})" class="edit text-muted p-2" id="editContact"><i class="fa-solid fa-pen"></i></button>
+                                <button onclick="deleteThisContact (${i})" class="delete text-muted p-2" id="deleteContact"><i class="fa-solid fa-trash"></i></button>
                               </div>
                             </div>
                           </div>
@@ -288,6 +299,70 @@ function displayContactList() {
   }
   rowData.innerHTML = box;
   //adding total contacts
-  addingTotalContact.innerHTML = `Manage and organize your ${totalNumberOfContacts} contacts`;
-  addTotalContactInHeader.innerHTML = `${totalNumberOfContacts}`;
+  addingTotalContact.innerHTML = `Manage and organize your ${contactList.length} contacts`;
+  addTotalContactInHeader.innerHTML = `${contactList.length}`;
+}
+
+function deleteThisContact(index) {
+  contactList.splice(index, 1);
+  localStorage.setItem("Contacts", JSON.stringify(contactList));
+  displayContactList();
+  // console.log(index);
+}
+var globalIndex = 0;
+function editContact(index) {
+  globalIndex = index;
+  showUpdateHideSaveBtn();
+  fullName.value = contactList[index].name;
+  phoneNumber.value = contactList[index].number;
+  emailAddress.value = contactList[index].email;
+  adress.value = contactList[index].adress;
+  contactGroup.value = contactList[index].contactGroup;
+  notes.value = contactList[index].notes;
+
+  // Get the modal id
+  var modalElement = document.getElementById("addContactModal");
+
+  // Get or create the Bootstrap Modal objext
+  var modalInstance = bootstrap.Modal.getOrCreateInstance(modalElement);
+  modalInstance.show();
+}
+
+function updateThisContact() {
+  var updatedContact = {
+    name: fullName.value,
+    number: phoneNumber.value,
+    email: emailAddress.value,
+    adress: adress.value,
+    adress: adress.value,
+    contactGroup: contactGroup.value,
+    notes: notes.value,
+  };
+  contactList[globalIndex] = updatedContact;
+
+  localStorage.setItem("Contacts", JSON.stringify(contactList));
+  displayContactList();
+  showSaveHideUpdateBtn();
+  clearAllInputs();
+
+  // to close button after adding contactts
+  if (document.activeElement) {
+    document.activeElement.blur();
+  }
+
+  // Hide the Bootstrap modal
+  var modalElement = document.getElementById("addContactModal");
+  var modalInstance = bootstrap.Modal.getInstance(modalElement);
+  if (modalInstance) {
+    modalInstance.hide();
+  }
+}
+
+function showUpdateHideSaveBtn() {
+  updateBtn.classList.remove("d-none");
+  saveBtn.classList.add("d-none");
+}
+function showSaveHideUpdateBtn() {
+  saveBtn.classList.remove("d-none");
+  updateBtn.classList.add("d-none");
 }
